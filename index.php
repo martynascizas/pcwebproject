20f9af17f2915ece605a8fd9a6d7231c25144e94
martynas.cizas.webdev <martynas.cizas.webdev@gmail.com>
2023-03-20 09:23:39 +0200

    init nav edit

--- index.php
+++ index.php
@@ -10,6 +10,8 @@
 </head>
 
 <body>
+    <a href="crud/index.html">CRUD</a>
+    <br>
     <?php
     // Connect to the database
     require 'db.php';
@@ -55,40 +57,40 @@
     }
 
     // KOMPIUTERIU PRIEDAI
-      // Retrieve kompiuteriu_priedai data from the database
-        // $sql = "SELECT * FROM `kompiuteriu_priedai`";
-        $sql = "SELECT m.id, m.pavadinimas, m.aprasymas, m.kaina, GROUP_CONCAT(mp.filename SEPARATOR ',') AS photos 
+    // Retrieve kompiuteriu_priedai data from the database
+    // $sql = "SELECT * FROM `kompiuteriu_priedai`";
+    $sql = "SELECT m.id, m.pavadinimas, m.aprasymas, m.kaina, GROUP_CONCAT(mp.filename SEPARATOR ',') AS photos 
     FROM kompiuteriu_priedai m 
     LEFT JOIN kompiuteriu_priedai_photos mp ON m.id = mp.kompiuteriu_priedai_id 
     GROUP BY m.id";
-        $result = mysqli_query($conn, $sql);
+    $result = mysqli_query($conn, $sql);
 
-        if (mysqli_num_rows($result) > 0) {
-            while ($row = mysqli_fetch_assoc($result)) {
-                // Display monitor info
-                echo "<h3>" . $row["pavadinimas"] . " " . $row["aprasymas"] . "\" kompiuteriu_priedai - " . $row["kaina"] . " EUR</h3>";
+    if (mysqli_num_rows($result) > 0) {
+        while ($row = mysqli_fetch_assoc($result)) {
+            // Display monitor info
+            echo "<h3>" . $row["pavadinimas"] . " " . $row["aprasymas"] . "\" kompiuteriu_priedai - " . $row["kaina"] . " EUR</h3>";
 
-                // Display photos
-                $photos = explode(",", $row["photos"]);
-                echo "<div class=\"card mb-3\">";
-                echo "<div class=\"row g-0\">";
-                echo "<div class=\"col-md-4 text-center\">";
-                foreach ($photos as $photo) {
-                    echo "<img src='crud/kompiuteriu_priedai/uploads/" . $photo . "' class='img-fluid' style='max-width: 30vw;'>";
-                }
-                echo "</div>";
-                echo "<div class=\"col-md-8\">";
-                echo "<div class=\"card-body\">";
-                // echo "<h5 class=\"card-title\">" . $row["pavadinimas"] . " " . $row["aprasymas"] . "\" monitor - " . $row["kaina"] . " EUR</h5>";
-                echo "</div>";
-                echo "</div>";
-                echo "</div>";
-                echo "</div>";
-                echo "<hr>";
+            // Display photos
+            $photos = explode(",", $row["photos"]);
+            echo "<div class=\"card mb-3\">";
+            echo "<div class=\"row g-0\">";
+            echo "<div class=\"col-md-4 text-center\">";
+            foreach ($photos as $photo) {
+                echo "<img src='crud/kompiuteriu_priedai/uploads/" . $photo . "' class='img-fluid' style='max-width: 30vw;'>";
             }
-        } else {
-            echo "<br> No kompiuteriu priedai found";
+            echo "</div>";
+            echo "<div class=\"col-md-8\">";
+            echo "<div class=\"card-body\">";
+            // echo "<h5 class=\"card-title\">" . $row["pavadinimas"] . " " . $row["aprasymas"] . "\" monitor - " . $row["kaina"] . " EUR</h5>";
+            echo "</div>";
+            echo "</div>";
+            echo "</div>";
+            echo "</div>";
+            echo "<hr>";
         }
+    } else {
+        echo "<br> No kompiuteriu priedai found";
+    }
     // Close the database connection
     mysqli_close($conn);
     ?>
