85cb66f73d99ff6df8d263acc106b7c454c9fc48
martynas.cizas.webdev <martynas.cizas.webdev@gmail.com>
2023-03-27 00:18:03 +0300

    bg fix

--- index.php
+++ index.php
@@ -23,15 +23,15 @@
     ?>
 
     <div class="first-container d-flex align-items-center justify-content-end vh-100">
-        <div class="second-container">
-            <h1 class="fw-bold">Lorem Ipsum</h1>
+        <div class="second-container text-black">
+            <h1 class="fw-bold mt-5">Lorem Ipsum</h1>
             <p>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, <br> consectetur, adipisci velit... There is no one who loves pain itself,<br> who seeks after it and wants to have it, <br> simply because it is pain..."</p>
-            <button class="btn btn-black">Plačiau</button>
+            <a href="#categorys-container" class="btn btn-black">Plačiau</a>
         </div>
     </div>
 
     <!-- Kategorijos -->
-    <div class="container mt-3 mb-3 custom-container">
+    <div id="categorys-container"class="container mt-3 mb-3 custom-container">
         <div class="row row-cols-1 row-cols-md-2 g-3">
             <div class="col-md-5">
                 <div class="card h-100 shadow rounded-0" style="background-image: url('assets/img/laptops.jpg'); background-size: cover; border: none;">
