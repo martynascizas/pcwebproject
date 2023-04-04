3141004e2c329eca661d2c87afa070710dd0839b
martynas.cizas.webdev <martynas.cizas.webdev@gmail.com>
2023-04-04 23:17:52 +0300

    some filterring

--- components/monitoriai.php
+++ components/monitoriai.php
@@ -29,22 +29,42 @@
                     <?php
                     // Construct SQL query with the selected gamintojas value
                     $sql = "SELECT m.id, m.gamintojas, m.ekrano_istrizaine, m.rezoliucija, m.lieciamas_ekranas, m.kaina, GROUP_CONCAT(mp.filename SEPARATOR ',') 
-            AS photos
-            FROM monitoriai m 
-            LEFT JOIN monitoriai_photos mp ON m.id = mp.monitoriai_id";
+                    AS photos
+                    FROM monitoriai m 
+                    LEFT JOIN monitoriai_photos mp ON m.id = mp.monitoriai_id";
 
-                    if (!empty($gamintojas) && empty($ekrano_istrizaine)) {
-                        $sql .= " WHERE gamintojas = '$gamintojas' GROUP BY m.id";
-                    } else if (empty($gamintojas) && !empty($ekrano_istrizaine)) {
-                        $sql .= " WHERE ekrano_istrizaine = '$ekrano_istrizaine' GROUP BY m.id";
-                    } else if (!empty($gamintojas) && !empty($ekrano_istrizaine)) {
-                        $sql .= " WHERE gamintojas = '$gamintojas' AND ekrano_istrizaine = '$ekrano_istrizaine' GROUP BY m.id";
-                    } else {
-                        $sql .= " GROUP BY m.id";
+                    if (!empty($gamintojas) || !empty($ekrano_istrizaine) || !empty($rezoliucija) || !empty($lieciamas_ekranas) || !empty($kaina)) {
+                        $sql .= " WHERE ";
+
+                        if (!empty($gamintojas)) {
+                            $sql .= "gamintojas = '$gamintojas' AND ";
+                        }
+
+                        if (!empty($ekrano_istrizaine)) {
+                            $sql .= "ekrano_istrizaine = '$ekrano_istrizaine' AND ";
+                        }
+
+                        if (!empty($rezoliucija)) {
+                            $sql .= "rezoliucija = '$rezoliucija' AND ";
+                        }
+
+                        if (!empty($lieciamas_ekranas)) {
+                            $sql .= "lieciamas_ekranas = '$lieciamas_ekranas' AND ";
+                        }
+
+                        if (!empty($kaina)) {
+                            $sql .= "kaina <= '$kaina' AND ";
+                        }
+
+                        $sql = rtrim($sql, "AND ");
                     }
+
+                    $sql .= " GROUP BY m.id";
 
                     // Execute query and fetch results
                     $result = mysqli_query($conn, $sql);
+
+                    // Generate select options for gamintojas
                     ?>
                     <label for="gamintojas" class="form-label">Gamintojas</label>
                     <select class="form-select" id="gamintojas" name="gamintojas">
@@ -91,76 +111,130 @@
                         ?>
                     </select>
 
+                    <label for="rezoliucija" class="form-label">Rezoliucija</label>
+                    <select class="form-select" id="rezoliucija" name="rezoliucija">
+                        <option value="">Visi</option>
+                        <?php
+                        // Reset the pointer of the result set to the beginning
+                        mysqli_data_seek($result, 0);
+
+                        // Loop through result set and generate options
+                        $selected_values = array();
+                        while ($row = mysqli_fetch_assoc($result)) {
+                            $selected = '';
+                            if ($_POST['rezoliucija'] == $row['rezoliucija']) {
+                                $selected = 'selected';
+                            }
+                            if (!in_array($row['rezoliucija'], $selected_values)) {
+                                echo '<option value="' . $row["rezoliucija"] . '" ' . $selected . '>' . $row["rezoliucija"] . '</option>';
+                                $selected_values[] = $row['rezoliucija'];
+                            }
+                        }
+                        ?>
+                    </select>
+
+                    <label for="lieciamas_ekranas" class="form-label">Lieciamas ekranas</label>
+                    <select class="form-select" id="lieciamas_ekranas" name="lieciamas_ekranas">
+                        <option value="">Visi</option>
+                        <?php
+                        // Reset the pointer of the result set to the beginning
+                        mysqli_data_seek($result, 0);
+
+                        // Loop through result set and generate options
+                        $selected_values = array();
+                        while ($row = mysqli_fetch_assoc($result)) {
+                            $selected = '';
+                            if ($_POST['lieciamas_ekranas'] == $row['lieciamas_ekranas']) {
+                                $selected = 'selected';
+                            }
+                            if (!in_array($row['lieciamas_ekranas'], $selected_values)) {
+                                echo '<option value="' . $row["lieciamas_ekranas"] . '" ' . $selected . '>' . $row["lieciamas_ekranas"] . '</option>';
+                                $selected_values[] = $row['lieciamas_ekranas'];
+                            }
+                        }
+                        ?>
+                    </select>
+
+                    <label for="kaina" class="form-label">Kaina iki (€)</label>
+                    <input type="number" class="form-control" id="kaina" name="kaina" min="0" max="9999" step="1" value="<?php echo isset($_POST['kaina']) ? $_POST['kaina'] : ''; ?>">
+
                 </div>
                 <button type="submit" class="btn btn-primary" name="filter_submit">Filtruoti</button>
             </form>
 
-            <!-- <?php
-                    // Check if form has been submitted
-                    if (isset($_POST['filter_submit'])) {
-                        // Get the selected gamintojas value from the form
-                        $gamintojas = $_POST['gamintojas'];
 
-                        // Construct SQL query with the selected gamintojas value
-                        $sql = "SELECT * FROM monitoriai";
-                        if (!empty($gamintojas)) {
-                            $sql .= " WHERE gamintojas = '$gamintojas'";
-                        }
+            <?php
+            // Check if form has been submitted
+            if (isset($_POST['filter_submit'])) {
+                // Get the selected gamintojas and ekrano_istrizaine values from the form
+                $gamintojas = $_POST['gamintojas'];
+                $ekrano_istrizaine = $_POST['ekrano_istrizaine'];
+                $rezoliucija = $_POST['rezoliucija'];
+                $lieciamas_ekranas = $_POST['lieciamas_ekranas'];
+                $kaina = $_POST['kaina'];
 
-                        // Execute query and fetch results
-                        $result = mysqli_query($conn, $sql);
-                    ?> -->
-        <?php
-                        // Check if form has been submitted
-                        if (isset($_POST['filter_submit'])) {
-                            // Get the selected gamintojas and ekrano_istrizaine values from the form
-                            $gamintojas = $_POST['gamintojas'];
-                            $ekrano_istrizaine = $_POST['ekrano_istrizaine'];
-
-                            // Construct SQL query with the selected gamintojas and ekrano_istrizaine values
-                            $sql =
-                                "SELECT m.id, m.gamintojas, m.ekrano_istrizaine, m.rezoliucija, m.lieciamas_ekranas, m.kaina, m.timestamp, GROUP_CONCAT(mp.filename SEPARATOR ',') 
-                            AS photos
+                // Construct SQL query with the selected gamintojas and ekrano_istrizaine values
+                $sql = "SELECT m.id, m.gamintojas, m.ekrano_istrizaine, m.rezoliucija, m.lieciamas_ekranas, m.kaina, m.timestamp, GROUP_CONCAT(mp.filename SEPARATOR ',') AS photos
                             FROM monitoriai m 
                             LEFT JOIN monitoriai_photos mp ON m.id = mp.monitoriai_id";
-                            if (!empty($gamintojas) && empty($ekrano_istrizaine)) {
-                                $sql .= " WHERE gamintojas = '$gamintojas' GROUP BY m.id";
-                            } else if (empty($gamintojas) && !empty($ekrano_istrizaine)) {
-                                $sql .= " WHERE ekrano_istrizaine = '$ekrano_istrizaine' GROUP BY m.id";
-                            } else if (!empty($gamintojas) && !empty($ekrano_istrizaine)) {
-                                $sql .= " WHERE gamintojas = '$gamintojas' AND ekrano_istrizaine = '$ekrano_istrizaine' GROUP BY m.id";
-                            } else {
-                                $sql .= " GROUP BY m.id";
-                            }
-                            // Execute query and fetch results
-                            $result = mysqli_query($conn, $sql);
-                            // Display results in HTML table
-                            if (mysqli_num_rows($result) > 0) {
-                                echo '<div class="row row-cols-1 row-cols-md-3 g-4">';
-                                while ($row = mysqli_fetch_assoc($result)) {
-                                    echo '<div class="col">';
-                                    echo '<div class="card h-100">';
-                                    echo '<img src="../crud/monitoriai/uploads/' . $row["photos"] . '" class="card-img-top" alt="Product Image">';
-                                    echo '<div class="card-body">';
-                                    echo '<h5 class="card-title">' . $row["gamintojas"] . '</h5>';
-                                    echo '<p class="card-text">' . $row["kaina"] . '</p>';
-                                    echo '<p class="card-text">' . $row["ekrano_istrizaine"] . '</p>';
-                                    echo '<p class="card-text">' . $row["rezoliucija"] . '</p>';
-                                    echo '<p class="card-text">' . $row["lieciamas_ekranas"] . '</p>';
-                                    echo '</div>';
-                                    echo '<div class="card-footer">';
-                                    echo '<small class="text-muted">' . $row["timestamp"] . '</small>';
-                                    echo '</div>';
-                                    echo '</div>';
-                                    echo '</div>';
-                                }
-                                echo '</div>';
-                            } else {
-                                echo "0 results";
-                            }
-                        }
+                if (!empty($gamintojas) && empty($ekrano_istrizaine) && empty($rezoliucija) && empty($lieciamas_ekranas)) {
+                    $sql .= " WHERE gamintojas = '$gamintojas' GROUP BY m.id";
+                    //gamintojas
+                } else if (empty($gamintojas) && !empty($ekrano_istrizaine) && empty($rezoliucija) && empty($lieciamas_ekranas)) {
+                    $sql .= " WHERE ekrano_istrizaine = '$ekrano_istrizaine' GROUP BY m.id";
+                    //ekrano_istrizaine
+                } else if (empty($gamintojas) && empty($ekrano_istrizaine) && !empty($rezoliucija) && empty($lieciamas_ekranas)) {
+                    $sql .= " WHERE rezoliucija = '$rezoliucija' GROUP BY m.id";
+                    //rezoliucija
+                } else if (empty($gamintojas) && empty($ekrano_istrizaine) && empty($rezoliucija) && !empty($lieciamas_ekranas)) {
+                    $sql .= " WHERE lieciamas_ekranas = '$lieciamas_ekranas' GROUP BY m.id";
+                    //lieciamas_ekranas
+                } else if (!empty($gamintojas) && !empty($ekrano_istrizaine) && empty($rezoliucija) && empty($lieciamas_ekranas)) {
+                    $sql .= " WHERE gamintojas = '$gamintojas' AND ekrano_istrizaine = '$ekrano_istrizaine' GROUP BY m.id";
+                    //gamintojas / ekrano_istrizaine
+                } else if (!empty($gamintojas) && empty($ekrano_istrizaine) && !empty($rezoliucija) && empty($lieciamas_ekranas)) {
+                    $sql .= " WHERE gamintojas = '$gamintojas' AND rezoliucija = '$rezoliucija' GROUP BY m.id";
+                    //gamintojas / rezoliucija
+                } else if (!empty($gamintojas) && empty($ekrano_istrizaine) && empty($rezoliucija) && !empty($lieciamas_ekranas)) {
+                    $sql .= " WHERE gamintojas = '$gamintojas' AND lieciamas_ekranas = '$lieciamas_ekranas' GROUP BY m.id";
+                    //gamintojas / lieciamas_ekranas
+                } else if (!empty($gamintojas) && !empty($ekrano_istrizaine) && !empty($rezoliucija) && empty($lieciamas_ekranas)) {
+                    $sql .= " WHERE gamintojas = '$gamintojas' AND ekrano_istrizaine = '$ekrano_istrizaine' AND rezoliucija = '$rezoliucija' GROUP BY m.id";
+                    //gamintojas /ekrano_istrizaine / rezoliucija
+                } else if (!empty($gamintojas) && !empty($ekrano_istrizaine) && !empty($rezoliucija) && !empty($lieciamas_ekranas)) {
+                    $sql .= " WHERE gamintojas = '$gamintojas' AND ekrano_istrizaine = '$ekrano_istrizaine' AND rezoliucija = '$rezoliucija' AND lieciamas_ekranas = '$lieciamas_ekranas' GROUP BY m.id";
+                    //gamintojas / ekrano_istrizaine / rezoliucija / lieciamas_ekranas
+                } else {
+                    $sql .= " GROUP BY m.id";
+                }
+                // Execute query and fetch results
+                $result = mysqli_query($conn, $sql);
+                // Display results in HTML table
+                if (mysqli_num_rows($result) > 0) {
+                    echo '<div class="row row-cols-1 row-cols-md-3 g-4">';
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        echo '<div class="col">';
+                        echo '<div class="card h-100">';
+                        echo '<img src="../crud/monitoriai/uploads/' . $row["photos"] . '" class="card-img-top" alt="Product Image">';
+                        echo '<div class="card-body">';
+                        echo '<h5 class="card-title">' . $row["gamintojas"] . '</h5>';
+                        echo '<p class="card-text">' . $row["kaina"] . '</p>';
+                        echo '<p class="card-text">' . $row["ekrano_istrizaine"] . '</p>';
+                        echo '<p class="card-text">' . $row["rezoliucija"] . '</p>';
+                        echo '<p class="card-text">' . $row["lieciamas_ekranas"] . '</p>';
+                        echo '</div>';
+                        echo '<div class="card-footer">';
+                        echo '<small class="text-muted">' . $row["timestamp"] . '</small>';
+                        echo '</div>';
+                        echo '</div>';
+                        echo '</div>';
                     }
-        ?>
+                    echo '</div>';
+                } else {
+                    echo "0 results";
+                }
+            }
+            ?>
         </div>
         <!-- footer -->
         <?php
