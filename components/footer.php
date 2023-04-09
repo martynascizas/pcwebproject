633b0fdfb3eb3d26935274dcd570b6512b434857
martynas.cizas.webdev <martynas.cizas.webdev@gmail.com>
2023-04-09 23:10:41 +0300

    footer fix

--- components/footer.php
+++ components/footer.php
@@ -2,24 +2,20 @@
   <div class="container">
     <div class="row">
       <div class="col-md-4 text-center">
-        <h4 class="text-white">APIE MUS</h4>
-        <p class="text-secondary">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer varius ex ut bibendum ornare. Sed vitae mattis arcu. Duis ut purus sed nibh condimentum interdum nec a ipsum. Maecenas suscipit tortor felis. Vestibulum in erat augue. Nullam quis sapien vulputate, finibus justo ac, bibendum neque. Fusce molestie convallis nulla et.</p>
+        <h6 class="text-white">APIE MUS</h6>
+        <p class="text-secondary">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
       </div>
       <div class="col-md-4 text-center">
-        <h4 class="text-white">MŪSŲ PASLAUGOS</h4>
+        <h6 class="text-white">MŪSŲ PASLAUGOS</h6>
         <ul class="list-unstyled">
-          <li class="text-secondary">Kompiuterių Pardavimas</li>
-          <li class="text-secondary">Kompiuterių supirkimas</li>
-          <li class="text-secondary">Remontas</li>
+          <li class="text-secondary">Kompiuterių ir jų priedų pardavimas</li>
         </ul>
       </div>
       <div class="col-md-4 text-center">
-        <h4 class="text-white">SUSISIEKTI</h4>
-        <ul class="list-unstyled">
-          <li class="text-secondary"><i class="bi bi-geo-alt"></i></li>
-          <li class="text-secondary"><i class="bi bi-telephone"></i></li>
-          <li class="text-secondary"><i class="bi bi-envelope"></i></li>
-        </ul>
+        <h6 class="text-white">SUSISIEKTI</h6>
+        <a class="text-secondary"><i class="bi bi-geo-alt "></i></a>
+        <a class="text-secondary"><i class="bi bi-telephone"></i></a>
+        <a class="text-secondary"><i class="bi bi-envelope"></i></a>
       </div>
     </div>
   </div>
