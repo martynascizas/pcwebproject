a11947f66549f414181e6cbcc02b2bb873200cdd
martynas.cizas.webdev <martynas.cizas.webdev@gmail.com>
2023-04-22 20:46:10 +0300

    akcijos

--- components/section1.php
+++ components/section1.php
@@ -1,42 +1,42 @@
-<section id="categorys-container">
+px<section id="categorys-container">
     <div id="heightfix" class=" d-flex flex-column align-items-center">
         <div class="container mt-1 custom-container flex-shrink-0 vh-100" style="width: 100vw; display: flex; flex-direction: column; justify-content: center;">
             <div class="row row-cols-1 row-cols-md-2 g-3">
                 <div class="col-md-6">
                     <div class="card h-100 shadow rounded-0" style="background-image: url('assets/img/laptops.jpg'); background-size: cover; border: none;">
-                        <div class="card-body" style="height: 300px;">
+                        <div class="card-body" style="height: 285px;">
                             <a href="components/nesiojami.php" class="btn btn-primary-white" style="position:absolute; bottom:20px; left:20px;">Nešiojami Kompiuteriai</a>
                         </div>
                     </div>
                 </div>
                 <div class="col-md-6">
                     <div class="card h-100 shadow rounded-0" style="background-image: url('assets/img/pc.webp'); background-size: cover; border: none;">
-                        <div class="card-body" style="height: 300px;">
+                        <div class="card-body" style="height: 285px;">
                             <a href="components/staliniai.php" class="btn btn-primary-white" style="position:absolute; bottom:20px; left:20px;">Staliniai Kompiuteriai</a>
                         </div>
                     </div>
                 </div>
+                <div class="col-md-12">
+                    <div class="card h-100 shadow rounded-0" style="background-image: url('assets/img/akcijos.jpg'); background-size: cover; border: none;">
+                        <div class="card-body" style="height: 60px;">
+                            <a href="components/akcijos.php" class="btn btn-primary-white" style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">Akcijos</a>
+                        </div>
+                    </div>
+                </div>
+
                 <div class="col-md-6">
                     <div class="card h-100 shadow rounded-0" style="background-image: url('assets/img/monitor.webp'); background-size: cover; border: none;">
-                        <div class="card-body" style="height: 300px;">
+                        <div class="card-body" style="height: 285px;">
                             <a href="components/monitoriai.php" class="btn btn-primary-white" style="position:absolute; bottom:20px; left:20px;">Monitoriai</a>
                         </div>
                     </div>
                 </div>
                 <div class="col-md-6">
                     <div class="card h-100 shadow rounded-0" style="background-image: url('assets/img/accessories.jpg'); background-size: cover; border: none;">
-                        <div class="card-body" style="height: 300px;">
+                        <div class="card-body" style="height: 285px;">
                             <a href="components/priedai.php" class="btn btn-primary-white" style="position:absolute; bottom:20px; left:20px;">Kompiuterių Priedai</a>
                         </div>
                     </div>
                 </div>
-                <!-- <div class="col-md-12">
-                    <div class="card h-100 shadow rounded-0" style="background-image: url('assets/img/akcijos.jpg'); background-size: cover; border: none;">
-                        <div class="card-body" style="height: 150px;">
-                            <a href="components/akcijos.php" class="btn btn-primary-white" style="position:absolute; bottom:20px; left:20px;">Akcijos</a>
-                        </div>
-                    </div>
-                </div> -->
             </div>
         </div>
-   
