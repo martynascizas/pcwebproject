8fd908d4cac826d29aee661ceeec50f69f1fa69e
martynas.cizas.webdev <martynas.cizas.webdev@gmail.com>
2023-04-24 21:00:28 +0300

    db sql structure update

--- assets/db_structure/computer_shop.sql
+++ assets/db_structure/computer_shop.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 07, 2023 at 08:51 PM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.2.0
+-- Generation Time: Apr 24, 2023 at 07:59 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,8 +18,37 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `computer_shop`
+-- Database: `testas`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `akcijos`
+--
+
+CREATE TABLE `akcijos` (
+  `id` int(11) NOT NULL,
+  `pavadinimas` varchar(50) NOT NULL,
+  `kaina` decimal(10,2) NOT NULL,
+  `nauja_kaina` decimal(10,2) NOT NULL,
+  `aprasymas` text DEFAULT NULL,
+  `photo` varchar(255) DEFAULT NULL,
+  `gamintojas` varchar(50) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `akcijos_photos`
+--
+
+CREATE TABLE `akcijos_photos` (
+  `id` int(11) NOT NULL,
+  `akcijos_id` int(11) NOT NULL,
+  `filename` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -37,14 +66,6 @@ CREATE TABLE `kompiuteriu_priedai` (
   `timestamp` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `kompiuteriu_priedai`
---
-
-INSERT INTO `kompiuteriu_priedai` (`id`, `pavadinimas`, `kaina`, `aprasymas`, `photo`, `gamintojas`, `timestamp`) VALUES
-(17, 'Rinkinys', '59.00', 'Priedo aprasymas', NULL, 'Deltaco', '2023-04-06 20:35:55'),
-(18, 'Rinkinys', '499.00', 'priedo aprasymas', NULL, 'AOC', '2023-04-07 16:12:00');
-
 -- --------------------------------------------------------
 
 --
@@ -56,14 +77,6 @@ CREATE TABLE `kompiuteriu_priedai_photos` (
   `kompiuteriu_priedai_id` int(11) NOT NULL,
   `filename` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `kompiuteriu_priedai_photos`
---
-
-INSERT INTO `kompiuteriu_priedai_photos` (`id`, `kompiuteriu_priedai_id`, `filename`) VALUES
-(45, 17, 'priedai.webp'),
-(46, 18, 'accessories.jpg');
 
 -- --------------------------------------------------------
 
@@ -82,15 +95,6 @@ CREATE TABLE `monitoriai` (
   `lieciamas_ekranas` enum('Yes','No') NOT NULL DEFAULT 'No'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `monitoriai`
---
-
-INSERT INTO `monitoriai` (`id`, `gamintojas`, `ekrano_istrizaine`, `kaina`, `photo`, `timestamp`, `rezoliucija`, `lieciamas_ekranas`) VALUES
-(79, 'AOC', 32, '299.00', NULL, '2023-04-06 19:37:51', 'Full HD', 'No'),
-(80, 'AOC', 27, '159.00', NULL, '2023-04-06 19:38:23', 'Full HD', 'No'),
-(81, 'AOC', 19, '15.00', NULL, '2023-04-07 16:11:47', 'Full HD', 'No');
-
 -- --------------------------------------------------------
 
 --
@@ -102,15 +106,6 @@ CREATE TABLE `monitoriai_photos` (
   `monitoriai_id` int(11) NOT NULL,
   `filename` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `monitoriai_photos`
---
-
-INSERT INTO `monitoriai_photos` (`id`, `monitoriai_id`, `filename`) VALUES
-(68, 79, 'aoc27.webp'),
-(69, 80, 'aoc27g2spu.jpg'),
-(70, 81, 'aoc27g2spu.jpg');
 
 -- --------------------------------------------------------
 
@@ -131,14 +126,6 @@ CREATE TABLE `nesiojami_kompiuteriai` (
   `timestamp` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `nesiojami_kompiuteriai`
---
-
-INSERT INTO `nesiojami_kompiuteriai` (`id`, `gamintojas`, `ekrano_istrizaine`, `procesorius`, `vaizdo_plokste`, `ram`, `hdd`, `kaina`, `photo`, `timestamp`) VALUES
-(21, 'Dell', 19, 'I5 8gen', 'AMD', 16, 500, '499.00', '', '2023-04-07 16:36:03'),
-(22, 'Mac Book Pro', 13, 'M2', 'M2', 16, 500, '2599.00', '', '2023-04-07 16:36:33');
-
 -- --------------------------------------------------------
 
 --
@@ -150,14 +137,6 @@ CREATE TABLE `nesiojami_kompiuteriai_photos` (
   `nesiojami_kompiuteriai_id` int(11) NOT NULL,
   `filename` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `nesiojami_kompiuteriai_photos`
---
-
-INSERT INTO `nesiojami_kompiuteriai_photos` (`id`, `nesiojami_kompiuteriai_id`, `filename`) VALUES
-(0, 21, 'del lap.jpg'),
-(0, 22, 'macbook_pro_13_in_silver_2.jpg');
 
 -- --------------------------------------------------------
 
@@ -177,14 +156,6 @@ CREATE TABLE `staliniai_kompiuteriai` (
   `timestamp` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `staliniai_kompiuteriai`
---
-
-INSERT INTO `staliniai_kompiuteriai` (`id`, `gamintojas`, `procesorius`, `vaizdo_plokste`, `ram`, `hdd`, `kaina`, `photo`, `timestamp`) VALUES
-(13, 'Dell', 'I5 8gen', '3060', 16, 240, '499.00', '', '2023-04-07 16:38:19'),
-(14, 'AOC', 'intel', '3060', 16, 240, '59.00', '', '2023-04-07 16:38:33');
-
 -- --------------------------------------------------------
 
 --
@@ -198,16 +169,21 @@ CREATE TABLE `staliniai_kompiuteriai_photos` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `staliniai_kompiuteriai_photos`
---
-
-INSERT INTO `staliniai_kompiuteriai_photos` (`id`, `staliniai_kompiuteriai_id`, `filename`) VALUES
-(0, 13, 'pc.webp'),
-(0, 14, 'pc.jfif');
-
---
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `akcijos`
+--
+ALTER TABLE `akcijos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `akcijos_photos`
+--
+ALTER TABLE `akcijos_photos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `akcijos_id` (`akcijos_id`);
 
 --
 -- Indexes for table `kompiuteriu_priedai`
@@ -252,6 +228,18 @@ ALTER TABLE `staliniai_kompiuteriai`
 --
 
 --
+-- AUTO_INCREMENT for table `akcijos`
+--
+ALTER TABLE `akcijos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `akcijos_photos`
+--
+ALTER TABLE `akcijos_photos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
 -- AUTO_INCREMENT for table `kompiuteriu_priedai`
 --
 ALTER TABLE `kompiuteriu_priedai`
@@ -279,17 +267,23 @@ ALTER TABLE `monitoriai_photos`
 -- AUTO_INCREMENT for table `nesiojami_kompiuteriai`
 --
 ALTER TABLE `nesiojami_kompiuteriai`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT for table `staliniai_kompiuteriai`
 --
 ALTER TABLE `staliniai_kompiuteriai`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `akcijos_photos`
+--
+ALTER TABLE `akcijos_photos`
+  ADD CONSTRAINT `akcijos_photos_ibfk_1` FOREIGN KEY (`akcijos_id`) REFERENCES `akcijos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `kompiuteriu_priedai_photos`
